a7673da811eb4258ef8456dd6c3f1206e794c234
D4nielBerg <danielberg863@gmail.com>
2022-10-15 16:13:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold font-sans">Olá</h1>
+      <button className="bg-cyan-500 rounded-[4px] p-2 hover:bg-cyan-300">Hello world</button>
+    </div>
+  )
+}
+
+export default App
